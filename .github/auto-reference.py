1a769b93e911455ae15abfaf0d653f764497557b
IfJay <andreajsm03@gmail.com>
2024-12-09 15:36:01 +0100

    asdhasjdjkasd

--- .github/auto-reference.py
+++ .github/auto-reference.py
@@ -5,7 +5,8 @@ import datetime
 import re
 
 DOCS_PATH = "Docs"
-G_KEYWORD = "\\textsuperscript{g}"
+G_KEYWORD = r"\\textsuperscript{g}"
+SKIP_LATEX_TAGS = [r"\\section",r"\\subsection",r"\\subsubsection",r"\\paragraph",r"\\includegraphics",r"\\url"]
 
 def main(UseThread:bool=False):
     glox_path = path.Path(DOCS_PATH+"/Generali/glossario/01_intro.tex")
@@ -17,19 +18,19 @@ def main(UseThread:bool=False):
             for type in os.listdir(path.Path(DOCS_PATH)):
                 if type == "Candidatura": # skippa la candidatura
                     continue
-                pool.submit(Apply_g,defs,type)
+                pool.submit(Apply,defs,type)
     else:
         ApplyAll(defs)
-        print(defs)
+        # print(defs)
 
 
 def ApplyAll(defs:list):
     for type in os.listdir(path.Path(DOCS_PATH)):
         if type == "Candidatura":
             continue
-        Apply_g(defs, type)
+        Apply(defs, type)
 
-def Apply_g(defs:list, type:str):
+def Apply(defs:list, type:str):
     for doc in os.listdir(path.Path(DOCS_PATH+"/"+type)):
         if doc == "glossario": # skippa il glossario
             continue
@@ -40,30 +41,40 @@ def Apply_g(defs:list, type:str):
         for file in  os.listdir(current_path): # for each file file in a doc
             if ".tex" not in file: # if is not a tex file skip
                 continue
-            with open(str(current_path)+"/"+file,'r+',encoding='utf-8') as f:
-                lines = f.readlines()
-                text = '$$'.join(lines)
-                lines = f.readlines()
-                for i,line in enumerate(lines): # for each line
-                    current_line = line.split()
-                    for word in current_line:   #for each word
-                        isItalicOrBold = "\\textbf" in word or "\\textit" in word
-                        if isItalicOrBold: # if it is \textbf or \textbf is a valid word anyway
-                                word = word[8:-1]
-                        if word.lower() in defs: # if word is in the definitions add pedice
-                            line = line.replace(word,word+"\\textsuperscript{g}")
-                            lines[i] = line
-                f.seek(0)
-                f.writelines(lines)
+            file_path = str(current_path)+"/"+file
+            Glossify(defs, file_path, file)
+
+def Glossify(defs, file_path, file):
+    with open(file_path,'r+',encoding='utf-8') as f:
+        lines = f.readlines()
+        for i,line in enumerate(lines):
+            if re.search(r"|".join(SKIP_LATEX_TAGS),line.strip()): # if is in the skip list continue
+                continue
+            for j,d in enumerate(defs): # test if any definition is present in a line and replace it with its unique code
+                pattern = "\\b"+d[0]+"\\b"
+                matched = re.search(pattern,line,flags=re.IGNORECASE) # if present store the initial word
+                if matched:
+                    m = matched.group()
+                    defs[j] = UpdateTupleKey(d,m) # update the lookup table
+                line = re.sub(pattern,d[1],line,flags=re.IGNORECASE) # replace with code
+                lines[i] = line
+            for d in defs: # replace codes with the original word+pedice
+                pattern = "\\b"+d[1]+"\\b"
+                line = re.sub(pattern,d[0]+G_KEYWORD,line,flags=re.IGNORECASE)
+                lines[i] = line
+        f.seek(0)
+        f.writelines(lines)
 
 def ReadAllWords(glox_path):
-    defs = []
+    defs = {}
+    i = 0
     with open(glox_path,'r',encoding='utf-8') as f:
         for line in f:
             if "\\subsection" in line:
                 g_def = line.strip()[12:-1]
-                defs.append(g_def.lower())
-    defs = sorted(defs, key=len,reverse=True)
+                defs[g_def] = str(i)*3
+                i+=1
+    defs = sorted(defs.items(), key=lambda x : len(x[0]),reverse=True) # sort dictionary based on word length
     return defs
 
 def IsRecent(doc:str):
@@ -74,6 +85,11 @@ def IsRecent(doc:str):
     document_date=datetime.datetime(int(date[0]),int(date[1]),int(date[2])) # date[0] = year, date[1] = month, date[2] = day
     return lower_bound<=document_date
 
+
+def UpdateTupleKey(t:tuple,value:str): # util function to overwrite a tuple
+    l = list(t)
+    l[0] = value
+    return tuple(l)
 
 if __name__ == "__main__":
     main()
